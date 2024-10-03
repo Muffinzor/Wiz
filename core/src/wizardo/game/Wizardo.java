1d8e6d20b62699176eed5cfe6bafb4acaad86c86
Leonardo Uliano <182024269+Muffinzor@users.noreply.github.com>
2024-10-03 14:59:09 -0400

    .gitignore

--- core/src/wizardo/game/Wizardo.java
+++ core/src/wizardo/game/Wizardo.java
@@ -7,25 +7,20 @@ import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Wizardo extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		System.out.println("Hello, World!");
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
